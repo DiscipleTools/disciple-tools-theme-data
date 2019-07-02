baa484e5c4544ec93ffa4995c6e6f9fb92dc492c
Chris Chasm <chris@chasm.solutions>
2019-07-02 13:51:10 -0600

    Changed create table

--- build_location_grid.php
+++ build_location_grid.php
@@ -6,6 +6,9 @@
  *          The full location_grid table installed in the database.
  *           ability to create/overwrite a new table called 'dt_location_grid'
  *           ability to save to a subfolder called /location_grid
+ *
+ *
+ *           php build_location_grid.php
  */
 
 // Extend PHP limits for large processing
@@ -81,7 +84,60 @@ print date('H:i:s') . ' | Begin.' . PHP_EOL;
 // create table
 print date('H:i:s') . ' | Start create table.' . PHP_EOL;
 $result = mysqli_query( $con, "
-CREATE TABLE `dt_location_grid` (   `grid_id` bigint(20) NOT NULL AUTO_INCREMENT,   `name` varchar(200) NOT NULL DEFAULT '',   `level` int(1) DEFAULT NULL,   `level_name` varchar(6) DEFAULT NULL,   `country_code` varchar(10) DEFAULT NULL,   `admin0_code` varchar(10) DEFAULT NULL,   `admin1_code` varchar(20) DEFAULT NULL,   `admin2_code` varchar(20) DEFAULT NULL,   `admin3_code` varchar(20) DEFAULT NULL,   `admin4_code` varchar(20) DEFAULT NULL,   `admin5_code` varchar(20) DEFAULT NULL,   `parent_id` bigint(20) DEFAULT NULL,   `admin0_grid_id` bigint(20) DEFAULT NULL,   `admin1_grid_id` bigint(20) DEFAULT NULL,   `admin2_grid_id` bigint(20) DEFAULT NULL,   `admin3_grid_id` bigint(20) DEFAULT NULL,   `admin4_grid_id` bigint(20) DEFAULT NULL,   `admin5_grid_id` bigint(20) DEFAULT NULL,   `longitude` float DEFAULT NULL,   `latitude` float DEFAULT NULL,   `north_latitude` float DEFAULT NULL,   `south_latitude` float DEFAULT NULL,   `west_longitude` float DEFAULT NULL,   `east_longitude` float DEFAULT NULL,   `population` bigint(20) NOT NULL DEFAULT '0',   `modification_date` date DEFAULT NULL,   `geonames_ref` bigint(20) DEFAULT NULL,   `wikidata_ref` varchar(20) DEFAULT NULL,   PRIMARY KEY (`grid_id`),   KEY `level` (`level`),   KEY `latitude` (`latitude`),   KEY `longitude` (`longitude`),   KEY `admin0_code` (`admin0_code`),   KEY `admin1_code` (`admin1_code`),   KEY `admin2_code` (`admin2_code`),   KEY `admin3_code` (`admin3_code`),   KEY `admin4_code` (`admin4_code`),   KEY `country_code` (`country_code`),   KEY `north_latitude` (`north_latitude`),   KEY `south_latitude` (`south_latitude`),   KEY `parent_id` (`parent_id`),   KEY `west_longitude` (`west_longitude`),   KEY `east_longitude` (`east_longitude`),   KEY `admin5_code` (`admin5_code`),   KEY `admin0_grid_id` (`admin0_grid_id`),   KEY `admin1_grid_id` (`admin1_grid_id`),   KEY `admin2_grid_id` (`admin2_grid_id`),   KEY `admin3_grid_id` (`admin3_grid_id`),   KEY `admin4_grid_id` (`admin4_grid_id`),   KEY `admin5_grid_id` (`admin5_grid_id`),   KEY `level_name` (`level_name`),   FULLTEXT KEY `name` (`name`) ) ENGINE=InnoDB AUTO_INCREMENT=100386738 DEFAULT CHARSET=utf8;
+CREATE TABLE `dt_location_grid` (
+   `grid_id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `name` varchar(200) NOT NULL DEFAULT '',
+    `level` float DEFAULT NULL,
+    `level_name` varchar(6) DEFAULT NULL,
+    `country_code` varchar(10) DEFAULT NULL,
+    `admin0_code` varchar(10) DEFAULT NULL,
+    `admin1_code` varchar(20) DEFAULT NULL,   
+    `admin2_code` varchar(20) DEFAULT NULL,  
+    `admin3_code` varchar(20) DEFAULT NULL,   
+    `admin4_code` varchar(20) DEFAULT NULL,   
+    `admin5_code` varchar(20) DEFAULT NULL,   
+    `parent_id` bigint(20) DEFAULT NULL,   
+    `admin0_grid_id` bigint(20) DEFAULT NULL,   
+    `admin1_grid_id` bigint(20) DEFAULT NULL,   
+    `admin2_grid_id` bigint(20) DEFAULT NULL,   
+    `admin3_grid_id` bigint(20) DEFAULT NULL,   
+    `admin4_grid_id` bigint(20) DEFAULT NULL,   
+    `admin5_grid_id` bigint(20) DEFAULT NULL,   
+    `longitude` float DEFAULT NULL,   
+    `latitude` float DEFAULT NULL,   
+    `north_latitude` float DEFAULT NULL,   
+    `south_latitude` float DEFAULT NULL,   
+    `west_longitude` float DEFAULT NULL,   
+    `east_longitude` float DEFAULT NULL,   
+    `population` bigint(20) NOT NULL DEFAULT '0',   
+    `modification_date` date DEFAULT NULL,   
+    `geonames_ref` bigint(20) DEFAULT NULL,   
+    `wikidata_ref` varchar(20) DEFAULT NULL,  
+     PRIMARY KEY (`grid_id`),   
+     KEY `level` (`level`),   
+     KEY `latitude` (`latitude`),   
+     KEY `longitude` (`longitude`),   
+     KEY `admin0_code` (`admin0_code`),   
+     KEY `admin1_code` (`admin1_code`),   
+     KEY `admin2_code` (`admin2_code`),  
+      KEY `admin3_code` (`admin3_code`),   
+      KEY `admin4_code` (`admin4_code`),   
+      KEY `country_code` (`country_code`),   
+      KEY `north_latitude` (`north_latitude`),   
+      KEY `south_latitude` (`south_latitude`),   
+      KEY `parent_id` (`parent_id`),   
+      KEY `west_longitude` (`west_longitude`),   
+      KEY `east_longitude` (`east_longitude`),   
+      KEY `admin5_code` (`admin5_code`),   
+      KEY `admin0_grid_id` (`admin0_grid_id`),   
+      KEY `admin1_grid_id` (`admin1_grid_id`),   
+      KEY `admin2_grid_id` (`admin2_grid_id`),   
+      KEY `admin3_grid_id` (`admin3_grid_id`),   
+      KEY `admin4_grid_id` (`admin4_grid_id`),   
+      KEY `admin5_grid_id` (`admin5_grid_id`),   
+      KEY `level_name` (`level_name`),   
+      FULLTEXT KEY `name` (`name`) ) 
+      ENGINE=InnoDB AUTO_INCREMENT=100386738 DEFAULT CHARSET=utf8;
     " );
 if ( ! $result ) {
     print date('H:i:s') . ' | Failed to create table' . PHP_EOL;
@@ -357,8 +413,9 @@ if ( ! $result ) {
 print date('H:i:s') . ' | Start dt_location_grid.tsv File Creation' . PHP_EOL;
 
 // Create Zip of dt_location_grid
-if ( file_exists( "location_grid/dt_location_grid.tsv" ) ) {
-    unlink("location_grid/dt_location_grid.tsv");
+if ( file_exists( "{$output['root']}/dt_location_grid.tsv" ) ) {
+    print 'unlink file' . PHP_EOL;
+    unlink("{$output['root']}/dt_location_grid.tsv");
 }
 
 $result = mysqli_query( $con, "
@@ -367,7 +424,9 @@ FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n';
     " );
 if ( ! $result ) {
+
     print date('H:i:s') . ' | FAIL: dt_location_grid.tsv file creation.' . PHP_EOL;
+    print_r($con);
     exit();
 } else {
     print date('H:i:s') . ' | End dt_location_grid.tsv file creation' . PHP_EOL;
@@ -390,7 +449,7 @@ print $zip->numFiles . PHP_EOL;
 $zip->close();
 
 if ( file_exists( "{$output['lg']}dt_location_grid.tsv.zip" ) ) {
-    unlink("{$output['lg']}dt_location_grid.tsv");
+    unlink("{$output['root']}dt_location_grid.tsv");
     print date('H:i:s') . ' | Removed .tsv file' . PHP_EOL;
     print date('H:i:s') . ' | End dt_location_grid.tsv.zip' . PHP_EOL;
 } else {
